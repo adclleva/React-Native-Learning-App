5c24456e5a58b5a82e4a866048da607df21590f3
Arvin Lleva <arvin.lleva@yahoo.com>
2020-01-13 23:46:51 -0800

    Utilized the View, Text, and Styles concepts for React Native

--- App.js
+++ App.js
@@ -3,12 +3,25 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.container}> 
+      <View style={styles.header}>
+        <Text style={styles.boldText}>Hello World!</Text>
+      </View>
+
+      <View style={styles.body}>
+        <Text style={styles.boldText}>Lorem ipsum dolor sit amet</Text>
+        <Text style={styles.boldText}>Lorem ipsum dolor sit amet</Text>
+        <Text style={styles.boldText}>Lorem ipsum dolor sit amet</Text>
+      </View>
     </View>
   );
 }
 
+// this emulates the idea of CSS
+// instead of using hyphens, we do camelCase
+// keep in mind that styles are not necessarily inherited as we need to add the
+//  style directly, nearly as inline style
+// unless it is a text widget, thus text components can inherite text component styles
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -16,4 +29,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    backgroundColor: 'pink',
+    padding: 20, // this is in pixels
+  },
+  boldText: {
+    fontWeight: 'bold'
+  },
+  body: {
+    backgroundColor: 'yellow',
+    padding: 20
+  }
+
 });
